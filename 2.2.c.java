5dc07145a7c6926664c443f2b6f719b6305c7722
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2021-10-13 18:27:46 +0200

    Update 2.2.c.java

--- 2.2.c.java
+++ 2.2.c.java
@@ -5,11 +5,11 @@ public class Start {
     public static void main(String[] args) {
         Scanner temp = new Scanner(System.in);
         int n = temp.nextInt();
-        boolean p = false;
-        if(sqrt(n) % 1 == 0)
-        {
-            p = true;
+        for (int i = 1; i < n; i++) {
+            if(sqrt(n) % 1 == 0)
+            {
+                System.out.println(i);
+            }
         }
-        System.out.println(p);
     }
 }
