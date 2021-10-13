06f42618b591ec7739a65537b598a37bd7382d4e
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2021-10-13 17:40:13 +0200

    Create 2.1.h.java

---
+++ 2.1.h.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+public class Start {
+
+    public static void main(String[] args) {
+        Scanner temp = new Scanner(System.in);
+        int n = temp.nextInt();
+        int wynik = 0;
+        for (int i = 0; i < n; i++) {
+            int t = temp.nextInt();
+            wynik = (int) (wynik + pow(-1, 1+i) * t);
+        }
+        System.out.println(wynik);
+    }
+}
