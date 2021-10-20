51925351a0c39b202dde6b7f902a7d2c5601523e
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2021-10-20 17:45:23 +0200

    Create 3.2.f.java

---
+++ 3.2.f.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Start {
+
+    public static void generuj(int tab[], int n, int min, int max)
+    {
+        Random r = new Random();
+        for (int j = 0; j < n; ++j) {
+            tab[j] = r.nextInt(max + 1 - min) + min;
+        }
+    }
+
+    public static void signum(int tab[])
+    {
+        int wyn = 0;
+        for(int i = 0; i< tab.length; i++)
+        {
+            if(tab[i] > 0)
+            {
+                tab[i] = 1;
+            }
+            if(tab[i] < 0)
+            {
+                tab[i] = -1;
+            }
+            if(tab[i] == 0)
+            {
+                tab[i] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int []tab = new int[5];
+        generuj(tab, 5, -10, 10);
+        signum(tab);
+        for(int i = 0; i<5; i++)
+        {
+            System.out.println(tab[i]);
+        }
+    }
+}
